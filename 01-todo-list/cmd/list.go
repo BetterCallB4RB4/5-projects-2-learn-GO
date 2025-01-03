c996bcef95e5559f0638bd8500544b43847f482d
Augusto Barbadoro <augusto.barbadoro244@gmail.com>
2025-01-03 17:54:19 +0100

    working on formatting the output text

--- 01-todo-list/cmd/list.go
+++ 01-todo-list/cmd/list.go
@@ -1,12 +1,16 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"text/tabwriter"
+	"time"
 
+	"github.com/mergestat/timediff"
 	"github.com/spf13/cobra"
 )
 
@@ -21,7 +25,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		printFormatterTaskList()
 	},
 }
 
@@ -37,4 +41,31 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func printFormatterTaskList() {
+	tabFilter := tabwriter.NewWriter(os.Stdout, 0, 0, 1, '.', tabwriter.AlignRight|tabwriter.Debug)
+	reader := createCsvReader(fileName)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		// dummy formatting, can be
+		id := record[0]
+		taskName := record[1]
+		isDone := record[3]
+
+		timeDate, err := time.Parse(time.RFC3339, record[2])
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		timeDiff := timediff.TimeDiff(timeDate)
+
+		fmt.Fprintln(tabFilter, id+"\t"+taskName+"\t"+timeDiff+"\t"+isDone+"\t")
+
+		tabFilter.Flush()
+	}
 }
