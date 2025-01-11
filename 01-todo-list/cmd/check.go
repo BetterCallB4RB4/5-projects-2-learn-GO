ea5f20606e0eb6f2395703549f370440cb1387f1
augusto barbadoro <augusto.barbadoro244@gmail.com>
2025-01-11 14:13:23 +0100

    add check commant, not working

--- 01-todo-list/cmd/check.go
+++ 01-todo-list/cmd/check.go
@@ -1,11 +1,12 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -21,7 +22,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("check called")
+		taskFlagOn, _ := cmd.Flags().GetString("task")
+		if taskFlagOn != "" {
+			taskId, err := strconv.Atoi(args[0])
+			if err != nil {
+				fmt.Println("Error:", err)
+				os.Exit(1)
+			}
+			checkTaskByID(taskId)
+			if err != nil {
+				fmt.Println("Error:", err)
+				os.Exit(1)
+			}
+		} else {
+		}
 	},
 }
 
@@ -37,4 +51,22 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// checkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	checkCmd.PersistentFlags().String("task", "t", "select a task by the name")
+}
+
+func checkTaskByID(taskId int) {
+	records := getCsvData()
+	records[taskId-1].done = true
+	writeCsv(records)
+}
+
+func checkTaskByString(taskString string) {
+	records := getCsvData()
+	for i, record := range records {
+		if record.task == taskString {
+			records[i].done = true
+		}
+	}
+	writeCsv(records)
 }
