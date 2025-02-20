1e2bb72bf3eea01ee4339a08892b82afbe9e3db8
Augusto Barbadoro <a.barbadoro@crif.com>
2025-02-20 09:46:25 +0100

    sync repo

--- 02-backend-api/main.go
+++ 02-backend-api/main.go
@@ -10,19 +10,29 @@ import (
 
 var logger *slog.Logger
 
+type Operation struct {
+	Number1 int `json:"number1"`
+	Number2 int `json:"number2"`
+}
+
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	logger.Info("init server")
 
 	router := http.NewServeMux()
-	router.HandleFunc("/", welcome)
 	err := http.ListenAndServe("localhost:8080", router)
 	if err != nil {
 		log.Fatal("internal server error")
 	}
+
+	// router.Handle("/add", handler http.Handler)
 }
 
 func welcome(w http.ResponseWriter, r *http.Request) {
 	logger.Info("received request at home endpoint", "method", r.Method, "url", r.URL)
 	fmt.Fprintf(w, "Hello, World!\n")
+}
+
+func addOperation(operand Operation) int {
+	return operand.Number1 + operand.Number2
 }
